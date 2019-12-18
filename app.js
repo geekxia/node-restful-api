502d40e48ed515eb4cbe0c41af48a3d96b43aa29
geekxia <448914712@qq.com>
2019-12-19 07:37:49 +0800

    1219

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('./db/connect')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var articleRouter = require('./routes/article')
+var uploadRouter = require('./routes/upload')
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/article', articleRouter)
+app.use('/upload', uploadRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
