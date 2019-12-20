75670ae105576d580a35cc5b7afd45a14a413ac0
geekxia <448914712@qq.com>
2019-12-20 18:00:26 +0800

    1220

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 ## 环境
 
 * 启动数据库服务`mongod --dbpath "D:\mongo\data"`
+* 项目启动：`npm start`
 * 使用 Robo3T 可视化工具操作数据库
 * 使用 Postman 测试接口
 
 
-## 接口文件
+## 接口文档
 
-* /user/regist 注册
-* /user/login  登录
+* POST /user/regist 注册
+* POST /user/login  登录
+* GET /user/getAllUsers  获取所有用户
 
-* /article/create 创建与编辑
+* POST /upload/img  图片上传
+
+* POST /article/create 创建与编辑
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var userModel = require('../db/userModel')
 
 /* 注册 */
-router.post('/regist', function(req, res, next) {
+router.post('/regist', function(req, res) {
   let { username, password, password2 } = req.body
   if(!username || !password || !password2) {
     res.json({err: 1, msg: '注册信息不能为空'})
@@ -27,14 +27,33 @@ router.post('/regist', function(req, res, next) {
 })
 
 /* 登录 */
-router.post('/login', function(req, res, next) {
+router.post('/login', function(req, res) {
   let { username, password } = req.body
+  // 使用token相关的算法库，来生成一个token
+    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
   userModel.find({username,password}).then(arr=>{
     if (arr.length > 0) {
-      res.json({err:0, msg:'登录成功'})
+      res.json({err:0, msg:'success', data: {token, username}})
     } else {
-      res.json({err:1, msg: '登录失败'})
+      res.json({err:1, msg: 'fail'})
     }
+  })
+})
+
+
+/* 获取用户列表：测试跨域 */
+router.get('/getAllUsers', function(req, res) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+
+  userModel.find().then(arr=>{
+    res.json({err:0, msg:'success', data: arr})
+  }).catch(err=>{
+    res.json({err:1, msg:'fail'})
   })
 })
 
